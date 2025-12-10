a612b08f8212105f570838b05885614f9d30c9ac
Daniel Ortiz <daniortizgaleano@gmail.com>
2025-12-10 12:08:03 +0000

    templates/ folder(created);starting flask html;uploads/ folder(provisional)

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import logging
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<p>Página principal del Gestor S3</p>"
+    return render_template("index.html")
 
 
 # get para mostrar formulario para seleccionar archivo,
 # post para enviar los datos al servidor, en este caso para subir
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_template("upload.html")
+    elif request.method == "POST":
+        message = ""
         f = request.files.get("the_file")
-        if f:
-            return f"Archivo recibido: {f.filename}"
-    else:
-        logging.warning("Intento de subir archivo sin enviar archivo")
-        return "Error: no se envió ningún archivo"
+        if f and f.filename != "":
+            secure_name = secure_filename(f.filename)
+            path = "uploads/" + secure_name
+            f.save(path)
+            message = f"File uploaded and saved: {secure_name}"
+            return render_template("upload.html", message=message)
+        else:
+            logging.warning("Attempted to upload without providing a file")
+            return "Error: Object not uploaded"
 
 
 @app.route("/download")
