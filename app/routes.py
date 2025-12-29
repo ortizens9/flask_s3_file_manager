4a0586fb1a6a9b09d8813ead8d9cecffd60b3ade
Daniel Ortiz <daniortizgaleano@gmail.com>
2025-12-29 17:01:51 +0000

    Objectives achieved: all templates & features finished'

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, render_template, request, current_app, send_file
 from werkzeug.exceptions import RequestEntityTooLarge
-from s3_service import download_file, upload_file, S3ServiceError
+from .s3_service import (
+    download_file,
+    upload_file,
+    S3ServiceError,
+    list_files,
+    list_s3buckets,
+    delete_file,
+    new_bucket,
+)
 import os
 
 routes = Blueprint("routes", __name__)
@@ -44,34 +52,95 @@ def upload():
             return render_template("upload.html", message="Error: Object not uploaded")
 
 
+@routes.route("/download")
+def download_wlink():
+    bucket = current_app.config["DEFAULT_BUCKET"]
+    try:
+        objects = list_files(bucket)
+        return render_template("download_wlink.html", objects=objects)
+    except Exception as e:
+        return f"Error: {e}"
+
+
 @routes.route("/download/<object_name>")
 def download(object_name):
     bucket = current_app.config["DEFAULT_BUCKET"]
-    os.makedirs("uploads", exist_ok=True)
-    file_path = os.path.join("uploads", object_name)
+    downloads_folder = os.path.join(current_app.root_path, "downloads")
+    os.makedirs(downloads_folder, exist_ok=True)
+    file_path = os.path.join(downloads_folder, object_name)
     # Para pulir necesitamos crear una función para borrar los archivos temporales.
     try:
         download_file(bucket, object_name, file_path)
         return send_file(file_path, as_attachment=True)
     except S3ServiceError as e:
-        return render_template("download.html", message=str(e))
+        return f"Error: {str(e)}", 500
 
 
-@routes.route("/delete")
+@routes.route("/delete", methods=["GET", "POST"])
 def delete():
-    return "<p>Ruta para borrar objeto</p>"
+    bucket = current_app.config["DEFAULT_BUCKET"]
+    objects = list_files(bucket)
+    if request.method == "GET":
+        return render_template("delete_file.html", objects=objects)
+    elif request.method == "POST":
+        obj_to_delete = request.form.get("object_name")
+        if obj_to_delete in objects:
+            try:
+                delete_file(bucket, obj_to_delete)
+                objects = list_files(bucket)
+                return render_template(
+                    "delete_file.html",
+                    message=f"Object '{obj_to_delete}' deleted successfully",
+                    objects=objects,
+                )
+            except S3ServiceError as e:
+                return render_template(
+                    "delete_file.html", message=str(e), objects=objects
+                )
+        else:
+            return render_template(
+                "delete_file.html",
+                message=f"Object '{obj_to_delete}' not found in bucket.",
+                objects=objects,
+            )
 
 
 @routes.route("/list")
 def list_objects():
-    return "<p>Ruta para listar objetos en un bucket</p>"
+    bucket = current_app.config["DEFAULT_BUCKET"]
+    objects = list_files(bucket)
+    return render_template("list_objects.html", objects=objects)
 
 
 @routes.route("/list_buckets")
-def list_buckets():
-    return "<p>Ruta para listar buckets</p>"
+def show_buckets():
+    buckets = list_s3buckets()
+    return render_template("list_buckets.html", buckets=buckets)
 
 
-@routes.route("/create_bucket")
+@routes.route("/create_bucket", methods=["GET", "POST"])
 def create_bucket():
-    return "<p>Ruta para crear bucket</p>"
+    buckets = list_s3buckets()
+    if request.method == "GET":
+        return render_template("create_bucket.html", buckets=buckets)
+    elif request.method == "POST":
+        nbucket_name = request.form.get("bucket_name")
+        if nbucket_name != "":
+            try:
+                new_bucket(nbucket_name)
+                buckets = list_s3buckets()
+                return render_template(
+                    "create_bucket.html",
+                    message=f"Bucket {nbucket_name} created succesfully.",
+                    buckets=buckets,
+                )
+            except S3ServiceError as e:
+                return render_template(
+                    "create_bucket.html", message=str(e), buckets=buckets
+                )
+        else:
+            return render_template(
+                "create_bucket.html",
+                message="Error creating new bucket.",
+                buckets=buckets,
+            )
--- app/s3_service.py
+++ app/s3_service.py
@@ -41,3 +41,32 @@ def download_file(bucket_name, object_name, file_path):
     except Exception as e:
         logging.error("Error downloading file from S3", exc_info=True)
         raise S3ServiceError(f"Failed to download file") from e
+
+
+def list_files(bucket_name):
+    s3 = S3Client()
+    try:
+        objects = s3.list_objects(bucket_name)
+        return objects
+    except Exception as e:
+        logging.error("Error listing the objects", exc_info=True)
+        raise S3ServiceError(f"Failed listing objects") from e
+
+
+def list_s3buckets():
+    s3 = S3Client()
+    try:
+        buckets = s3.list_buckets()
+        return buckets
+    except Exception as e:
+        logging.error("Error listing buckets", exc_info=True)
+        raise S3ServiceError(f"Failed listing buckets") from e
+
+
+def delete_file(bucket_name, object_name):
+    s3 = S3Client()
+    try:
+        s3.delete_object(bucket_name, object_name)
+    except Exception as e:
+        logging.error("Error deleting object", exc_info=True)
+        raise S3ServiceError(f"Failed deleting object") from e
